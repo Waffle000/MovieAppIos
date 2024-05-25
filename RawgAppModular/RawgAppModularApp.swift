627d5e4c6c51fde009f9b0115b0af6356638caf6
Waffle <enrico.maricar@gmail.com>
2024-05-26 00:46:40 +0700

    Initial Commit

---
+++ RawgAppModular/RawgAppModularApp.swift
@@ -0,0 +1,17 @@
+//
+//  RawgAppModularApp.swift
+//  RawgAppModular
+//
+//  Created by Enrico Maricar on 26/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RawgAppModularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
